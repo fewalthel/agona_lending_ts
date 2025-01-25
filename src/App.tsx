d312454c09c8af91da56b6ea3bc91b7d26a72e5d
fewalthel <fewalthel@gmail.com>
2025-01-25 09:35:57 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import {Header} from "./components/Header/Header.tsx";
+import {Footer} from "./components/Footer/Footer.tsx";
+
+export function App() {
+    return (
+        <>
+            <Header/>
+            <Footer/>
+        </>
+    )
+}
+
+
+
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+/*import styles from './Header.module.sass';*/
+import React from 'react';
+
+export const Header: React.FC = () => {
+    return (
+        <>
+            <header>header</header>
+        </>
+    )
+}
