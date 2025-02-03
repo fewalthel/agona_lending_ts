0847a85cc30b952b38e7f09c70783ec07fccb281
fewalthel <fewalthel@gmail.com>
2025-02-03 03:00:20 +0300

    final update

--- src/components/Sections/DecorSection/DecorSection.tsx
+++ src/components/Sections/DecorSection/DecorSection.tsx
@@ -1,14 +1,17 @@
 import './DecorSection.css'
 import React from "react";
 import { BottomRunningLine } from "../../RunningLines/BottomRunningLine.tsx";
-import { Picture } from "../../Picture/Picture.tsx";
-import { Pictures } from "../../Picture/pictures.ts";
 
 export const DecorSection: React.FC = () => {
     return (
         <section className="section decor-section">
-            <BottomRunningLine />
-            <Picture {...Pictures[8]} />
+            <BottomRunningLine/>
+            <picture>
+                <source className="bottom-foreign-text" media="(min-width: 414px)"
+                        srcSet="assets/images/vector/foreign_text_on_background_desktop.svg"/>
+                <img className="bottom-foreign-text" src="assets/images/vector/foreign_text_on_background_mobile.svg"
+                     alt="foreign text on green background"/>
+            </picture>
         </section>
     )
 }
--- src/components/Sections/ProductsSection/ProductsSection.tsx
+++ src/components/Sections/ProductsSection/ProductsSection.tsx
@@ -4,27 +4,32 @@ import { Products, IProductCard } from '../../ProductCards/products.ts';
 import { ProductCard } from "../../ProductCards/ProductCard.tsx";
 import { RunningLine } from "../../RunningLines/RunningLine.tsx";
 import { Fires } from "../../Fires/Fires.tsx";
-import { Picture } from "../../Picture/Picture.tsx";
-import { Pictures } from "../../Picture/pictures.ts";
 
 export const ProductsSection: React.FC = () => {
     return (
-            <section className="section products-section">
-                <div className="container container-for-slogan">
-                    <Picture {...Pictures[3]} />
-                    <p>THROW SOME LAVA<br/>IN YOUR JAVA</p>
-                    <Picture  {...Pictures[4]} />
+        <section className="section products-section">
+            <div className="container container-for-slogan">
+                <img src="assets/images/vector/scorpion_looks_right.svg" alt="picture of scorpion which looks right"/>
+                <p>THROW SOME LAVA<br/>IN YOUR JAVA
+                </p>
+                <img src="assets/images/vector/scorpion_looks_left.svg" alt="picture of scorpion which looks left"/>
+            </div>
+            <div className="container-for-products-cards-and-fires">
+                <Fires/>
+                <div className="container-for-cards">
+                    {Products.map((product: IProductCard) => (
+                        <ProductCard {...product} key={product.name}/>
+                    ))}
                 </div>
-                <div className="container-for-products-cards-and-fires">
-                    <Fires/>
-                    <div className="container-for-cards">
-                        {Products.map((product: IProductCard) => (
-                            <ProductCard {...product} key={product.name} />
-                        ))}
-                    </div>
-                </div>
-                <RunningLine repeats={10} text={'put some fire in your belly'} id={'container-for-average-running-line'}/>
-                <Picture {...Pictures[5]}/>
-            </section>
+            </div>
+            <RunningLine repeats={10} text={'put some fire in your belly'} id={'container-for-average-running-line'}/>
+            <picture>
+                <source className="products-section-img" media="(min-width: 1440px)"
+                        srcSet="assets/images/large/syrup_in_cup_large.jpg"/>
+                <source className="products-section-img" media="(min-width: 1024px)"
+                        srcSet="assets/images/medium/syrup_in_cup_medium.jpg"/>
+                <img className="products-section-img" src="assets/images/small/syrup_in_cup_small.jpg" alt="syrup in cup"/>
+            </picture>
+        </section>
     )
 }
